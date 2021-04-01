294b3784cabae70f172c1e1819d4e306bb221041
SuperPox <calebkohl@gmail.com>
2021-03-31 20:56:28 -0600

    adds sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,22 @@
 Rails.application.routes.draw do 
   
   resources (:users) do
-    resources :projects
+    resources (:projects)
   end
 
   resources (:projects) do
-    resources :tasks
+    resources (:tasks)
   end
 
   resources :tasks
 
-  ############################
-
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/signup', to: "users#new", as: "signup"
   post '/signup', to: "users#create"
   get '/login', to: "sessions#new", as: "login"
   post '/login', to: "sessions#create"
   post '/logout', to: "sessions#destroy"
   get "/auth/facebook/callback", to: "sessions#create_with_fb"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
   
 end
