168b23c216b1a372e5de44d65fc39de413c00f28
SuperPox <calebkohl@gmail.com>
2021-03-31 20:13:09 -0600

    begins login setup

--- Gemfile
+++ Gemfile
@@ -25,6 +25,15 @@ gem 'jbuilder', '~> 2.7'
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
 
+# CUSTOM ////////////////////////
+gem 'dotenv-rails'
+gem 'omniauth'
+gem 'omniauth-facebook'
+gem 'bcrypt'
+gem 'pry'
+# //////////////////////////////
+
+
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,23 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
+  
+  resources (:users) do
+    resources :projects
+  end
+
+  resources (:projects) do
+    resources :tasks
+  end
+
   resources :tasks
-  resources :projects
-  resources :users
+
+  ############################
+
+  get '/signup', to: "users#new", as: "signup"
+  post '/signup', to: "users#create"
+  get '/login', to: "sessions#new", as: "login"
+  post '/login', to: "sessions#create"
+  post '/logout', to: "sessions#destroy"
+  get "/auth/facebook/callback", to: "sessions#create_with_fb"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
